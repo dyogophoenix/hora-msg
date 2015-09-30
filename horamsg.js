c3dea6f82726142cc0db08ba96c442e78c4e2acb
Dyogo Phoenix <perfil@dyogophoenix.com.br>
2015-09-30 13:58:08 -0300

    Pequena Correção
    Corrigindo espaçamentos irregulares do script para um melhor alinhamento
    do código !

--- horamsg.js
+++ horamsg.js
@@ -1,10 +1,10 @@
 /*!  
-    Nome: Hora MSG
-    Versão: 1.0
-    Criado em: 30/09/2015
-    Autor: Dyogo Phoenix
-    Autor URL: http://dyogophoenix.com.br/
-    Licença: MIT License (MIT)
+	Nome: Hora MSG
+	Versão: 1.0
+	Criado em: 30/09/2015
+	Autor: Dyogo Phoenix
+	Autor URL: http://dyogophoenix.com.br/
+	Licença: MIT License (MIT)
 	Copyright (c) 2015 Dyogo Phoenix
 */
 
@@ -13,14 +13,9 @@
 	this.horaMSG = function(container, custom) {
 		var element = horaMSG.hasContainer( container );
 		if(element['Next'] > 0) {
-
 			var defaults = horaMSG.methods();
-			var config   = horaMSG.extends(defaults, custom);
-
-			horaMSG.finish( element, config );
-
-		} else {
-
+			var methods  = horaMSG.extends(defaults, custom);
+			horaMSG.finish( element, methods );
 		}
 	}
 
@@ -34,10 +29,10 @@
 
 	horaMSG.methods = function() {
 		var defaults = {
-				Madrugada : 'Boa Madrugada',
-				Dia       : 'Bom dia',
-				Tarde     : 'Boa tarde',
-				Noite     : 'Boa noite'
+			Madrugada : 'Boa Madrugada',
+			Dia       : 'Bom dia',
+			Tarde     : 'Boa tarde',
+			Noite     : 'Boa noite'
 		};
 		return defaults;
 	}
@@ -45,42 +40,32 @@
 	horaMSG.extends = function(defaults, options) {
 		if(typeof options == 'object') {
 			for (var key in defaults) {
-		        if(typeof options[key] != 'undefined') {
-		        	defaults[key] = options[key];		        	
-		        }
+				if(typeof options[key] != 'undefined') {
+					defaults[key] = options[key];		        	
+				}
 			}
 		}
 		return defaults;
 	}
 
-	horaMSG.finish = function( element, custom ) {
-
-		var container = element['Selector'];
-		
+	horaMSG.finish = function( element, method ) {
+		var container = element['Selector'];		
 		var data = new Date();
-    	var horario = data.getHours();
-
-    	var i = 0;
-	    var total = container.length;
-	    do {
-	        if (horario >= 0 && horario < 5) {
-	            container[i].innerHTML = custom['Madrugada'];
-	        }
-	        else if (horario >= 5 && horario < 12) {
-	            container[i].innerHTML = custom['Dia'];
-	        }
-	        else if (horario >= 12 && horario < 18) {
-	            container[i].innerHTML = custom['Tarde'];
-	        }
-	        else if (horario >= 18 && horario <= 23) {
-	            container[i].innerHTML = custom['Noite'];
-	        }
-	        else { }    
-	        i++;
-	    } while( i < total );
-
-	    setTimeout(function() { horaMSG.finish( element, custom ) }, 1000 );
-
+		var horario = data.getHours();
+		var i = 0;  var total = container.length;
+		do {
+			if (horario >= 0 && horario < 5) {
+				container[i].innerHTML = method['Madrugada'];
+			} else if (horario >= 5 && horario < 12) {
+				container[i].innerHTML = method['Dia'];
+			} else if (horario >= 12 && horario < 18) {
+				container[i].innerHTML = method['Tarde'];
+			} else if (horario >= 18 && horario <= 23) {
+				container[i].innerHTML = method['Noite'];
+			} else { }    
+			i++;
+		} while( i < total );
+		setTimeout(function() { horaMSG.finish( element, method ) }, 1000 );
 	}
 
 })();
